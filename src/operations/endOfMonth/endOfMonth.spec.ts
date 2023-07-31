a41236baa8520e8d0065e07ce70bd5328a12fc74
Karl Eckberg <karl@eckberg.se>
2023-07-31 20:04:17 +0200

    fixed bug in endofmonth

--- src/operations/endOfMonth/endOfMonth.spec.ts
+++ src/operations/endOfMonth/endOfMonth.spec.ts
@@ -3,4 +3,5 @@ import endOfMonth from ".";
 test("endOfMonth", () => {
   expect(endOfMonth(new Date("2021-11-24"))).toEqual(new Date("2021-11-30"));
   expect(endOfMonth(new Date("2021-02-01"))).toEqual(new Date("2021-02-28"));
+  expect(endOfMonth(new Date("2023-08-31"))).toEqual(new Date("2023-08-31"));
 });
--- src/operations/endOfMonth/index.ts
+++ src/operations/endOfMonth/index.ts
@@ -1,8 +1,8 @@
 export default function endOfMonth(referenceDate: Date = new Date()) {
   const date = new Date(+referenceDate);
 
-  date.setUTCMonth(date.getUTCMonth() + 1);
   date.setUTCDate(1);
+  date.setUTCMonth(date.getUTCMonth() + 1);
   date.setUTCDate(date.getUTCDate() - 1);
 
   return date;
